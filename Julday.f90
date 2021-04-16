e9e07d5b6fc8a70b79367ea68065a489207bdac9
Lucas <lucas.garofolo@gmail.com>
2021-04-16 15:10:12 -0300

    Update Julday.f90

--- Julday.f90
+++ Julday.f90
@@ -1,26 +1,26 @@
-      FUNCTION JULDAY(mm,id,iyyy,CALENDAR)
-	  !DETERMINA O DIA DO CALENDÁRIO JULIANO CORRESPONDENTE AO DIA, MÊS E ANO DADOS
-      INTEGER julday,id,iyyy,mm,IGREG,CALENDAR
-      PARAMETER (IGREG=15+31*(10+12*1582))
-      INTEGER ja,jm,jy
+FUNCTION JULDAY(mm,id,iyyy,CALENDAR)
+!DETERMINA O DIA DO CALENDÁRIO JULIANO CORRESPONDENTE AO DIA, MÊS E ANO DADOS
+INTEGER julday,id,iyyy,mm,IGREG,CALENDAR
+PARAMETER (IGREG=15+31*(10+12*1582))
+INTEGER ja,jm,jy
 
 IF(CALENDAR .EQ. 366) THEN
-      jy=iyyy
-      if (jy.eq.0) pause 'julday: there is no year zero'
-      if (jy.lt.0) jy=jy+1
-      if (mm.gt.2) then
+    jy=iyyy
+    if (jy.eq.0) pause 'julday: there is no year zero'
+    if (jy.lt.0) jy=jy+1
+    if (mm.gt.2) then
         jm=mm+1
-      else
+    else
         jy=jy-1
         jm=mm+13
-      endif
-      julday=int(365.25*jy)+int(30.6001*jm)+id+1720995
-      if (id+31*(mm+12*iyyy).ge.IGREG) then
+    endif
+    julday=int(365.25*jy)+int(30.6001*jm)+id+1720995
+    if (id+31*(mm+12*iyyy).ge.IGREG) then
         ja=int(0.01*jy)
         julday=julday+2-ja+int(0.25*ja)
-      endif
+     endif
 else
-julday=(iyyy-1)*360+(mm-1)*30+id
+    julday=(iyyy-1)*360+(mm-1)*30+id
 ENDIF
-      return
-      END
+return
+END
